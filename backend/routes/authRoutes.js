87f81a08f0b0e9180c46df3c4c3e1bd6ae713b9c
chadunivihanga48 <vihangadmc@gmail.com>
2026-03-28 21:31:11 +0530

    new commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,6 +7,8 @@ const {
     getUserInfo,
 } = require("../controllers/authController");
 
+const upload = require("../middleware/uploadMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
@@ -15,4 +17,12 @@ router.post("/login", loginUser);
 
 router.get("/getUser", protect, getUserInfo);
 
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename
+        }`;
+    res.status(200).json({ imageUrl });
+});
 module.exports = router;
